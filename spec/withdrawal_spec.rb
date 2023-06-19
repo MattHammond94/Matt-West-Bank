fa1522139d3590a0c925e5f93b2c559162c1b634
Matt Hammond <mjhammond94@gmail.com>
2023-06-19 16:05:40 +0100

    Add test cases for withdrawal class - Tests pass

---
+++ spec/withdrawal_spec.rb
@@ -0,0 +1,29 @@
+require 'withdrawal'
+require 'date'
+
+describe  do
+  context 'Basic functionality' do
+    it 'Should return the correct amount when amount is called' do
+      withdrawal1 = Withdrawal.new(100.00)
+      expect(withdrawal1.amount).to eq(100.00)
+    end
+
+    it 'Should return the correct date when date is called' do
+      withdrawal1 = Withdrawal.new(100.00)
+      date = Date.today
+      expect(withdrawal1.date).to eq(date)
+    end
+  end
+
+  context 'Fails/Error handling' do
+    it 'Should return an error if amount entered is not a float type' do
+      expect { withdrawal1 = Withdrawal.new(10) }.to raise_error "Amount entered is invalid."
+      expect { withdrawal2 = Withdrawal.new("10") }.to raise_error "Amount entered is invalid."
+      expect { withdrawal3 = Withdrawal.new(nil) }.to raise_error "Amount entered is invalid."
+    end
+
+    it 'Should return an error if amount entered is less than 0' do
+      expect { withdrawal1 = Withdrawal.new(-12.00) }.to raise_error "Amount entered is invalid."
+    end
+  end
+end

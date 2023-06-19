1ea184d9270c2fecf7764629670219c7383c9bb5
Matt Hammond <mjhammond94@gmail.com>
2023-06-19 14:44:54 +0100

    Add all files - Add unit test for deposit class

---
+++ spec/deposit_spec.rb
@@ -0,0 +1,10 @@
+require 'deposit'
+
+describe Deposit do
+  context 'Basic functionality' do
+    it 'Should return the correct amount when amount is called' do
+      deposit1 = Deposit.new(100.00)
+      expect(desposit1.amount).to eq(100.00)
+    end
+  end
+end

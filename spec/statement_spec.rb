6ad97654c641a1b10e129ca7016498a02ec5305c
Matt Hammond <mjhammond94@gmail.com>
2023-06-20 14:01:15 +0100

    Update statement spec to mock dependencies

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -11,7 +11,7 @@ describe Statement do
 
     it 'Should print the correct header when print_header is called' do
       io = double :io
-      depo1 = Deposit.new(200.00)
+      depo1 = double(:deposit, amount: 200.00, date: "10/10/2023")
       expect(io).to receive(:puts).with("date || credit || debit || balance").ordered
       statement1 = Statement.new(io)
       statement1.add_deposit(depo1)
@@ -20,9 +20,9 @@ describe Statement do
 
     it 'Should add deposit objects to the transactions array when add_deposit is called' do
       statement1 = Statement.new(Kernel)
-      depo1 = Deposit.new(100.00)
-      depo2 = Deposit.new(200.00)
-      depo3 = Deposit.new(50.95)
+      depo1 = double(:deposit, amount: 100.00, date: "10/10/2023")
+      depo2 = double(:deposit, amount: 200.00, date: "11/11/2023")
+      depo3 = double(:deposit, amount: 50.95, date: "12/12/2023")
       statement1.add_deposit(depo1)
       statement1.add_deposit(depo2)
       statement1.add_deposit(depo3)
@@ -33,7 +33,7 @@ describe Statement do
   context 'Fail/Error scenarios' do
     it 'Should return an error when a withdrawal value higher than current deposit value is attempted' do
       statement1 = Statement.new(Kernel)
-      withd1 = Withdrawal.new(100.00)
+      withd1 = double(:withdrawal, amount: 200.00, date: "08/08/2023")
       expect { statement1.take_withdrawal(withd1) }.to raise_error 'Cannot make a withdrawal due to insufficient funds'
     end
   end
